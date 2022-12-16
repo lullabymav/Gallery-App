427594b292e6f6911169aecc39330d9626adc176
syfirawdy <syafirawidiyanti11@gmail.com>
2022-12-16 23:14:51 +0700

    commit assignment

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "SYAFIRA WIDIYANTI" // TODO: replace this
+const studentId = "FE2994606" // TODO: replace this
+
+export { url, studentName, studentId }
---
+++ src/routes/Photos.jsx
@@ -0,0 +1,117 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Card from "../components/Card";
+
+const Photos = () => {
+  const [photos, setPhotos] = useState([]);
+  const [sort, setSort] = useState("asc");
+  const [submited, setSubmited] = useState("");
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [refresh, setRefresh] = useState(false)
+
+  const fetchData = async () => {
+		setLoading(true);
+		try {
+			const response = await fetch("https://gallery-app-server.vercel.app/photos");
+			const data = await response.json();
+			setPhotos(data);
+		} catch (err) {
+			setError(err);
+		}
+		setLoading(false);
+	};
+
+  const sortData = async () => {
+		setLoading(true);
+		try {
+			const response = await fetch(
+				"https://gallery-app-server.vercel.app/photos?_sort=id&_order=desc&q=test"
+			);
+			const data = await response.json();
+			setPhotos(data);
+		} catch (err) {
+			setError(err);
+		}
+		setLoading(false);
+	};
+
+  const deletePhoto = async (id) => {
+		const response = await fetch(`https://gallery-app-server.vercel.app/photos/${id}`, {
+			method: "DELETE"
+		});
+    //setRefresh(true)
+		// const data = await response.json();
+		// const expectedCaptions = data
+		// .filter((photo) => photo.id !== id)
+    //   	.map((photo) => photo.captions)
+		// setPhotos(await expectedCaptions)
+	};
+
+  useEffect(() => {
+		setLoading(true);
+		sortData();
+	}, [sort, submited]);
+
+  useEffect(() => {
+		setLoading(true);
+		fetchData();
+	}, []);
+
+  if (error) return <h1 style={{ width: "100%", textAlign: "center", marginTop: "20px" }} >Error!</h1>;
+
+  return (
+    <>
+      <div className="container">
+        <div className="options">
+          <select
+            onChange={(e) => setSort(e.target.value)}
+            data-testid="sort"
+            className="form-select"
+            style={{}}
+          >
+            <option value="asc">Ascending</option>
+            <option value="desc">Descending</option>
+          </select>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              setSubmited(search);
+            }}
+          >
+            <input
+              type="text"
+              data-testid="search"
+              onChange={(e) => setSearch(e.target.value)}
+              className="form-input"
+            />
+            <input
+              type="submit"
+              value="Search"
+              data-testid="submit"
+              className="form-btn"
+            />
+          </form>
+        </div>
+        <div className="content">
+          {loading ? (
+            <h1
+              style={{ width: "100%", textAlign: "center", marginTop: "20px" }}
+            >
+              Loading...
+            </h1>
+          ) : (
+            photos.map((photo) => {
+              return (
+                <Card key={photo.id} photo={photo} deletePhoto={deletePhoto} />
+              );
+            })
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Photos;
