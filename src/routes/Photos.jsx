eb652d94359660651353b4060415b156b44cba4c
syfirawdy <syafirawidiyanti11@gmail.com>
2022-12-16 23:44:53 +0700

    commit assignment

--- src/routes/Photos.jsx
+++ src/routes/Photos.jsx
@@ -12,7 +12,6 @@ const Photos = () => {
   //const [refresh, setRefresh] = useState(false)
 
   const fetchData = async () => {
-		setLoading(true);
 		try {
 			const response = await fetch("https://gallery-app-server.vercel.app/photos");
 			const data = await response.json();
@@ -24,7 +23,6 @@ const Photos = () => {
 	};
 
   const sortData = async () => {
-		setLoading(true);
 		try {
 			const response = await fetch(
 				"https://gallery-app-server.vercel.app/photos?_sort=id&_order=desc&q=test"
